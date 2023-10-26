bf26ffa2061017af46f1bec7d1bc63088a5657c1
noemi1234567 <noemi.leuenberger@stud.fhgr.ch>
2023-10-26 11:49:04 +0200

    Dropdown Unterseiten
    Dropdown funktioniert nun auf allen Unterseiten (dank Verlinkungen mit separaten js-Dateien)

---
+++ js/artikel.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownButton = document.querySelector(".dropdown .dropbtn");
+    const dropdownContent = document.querySelector(".dropdown .dropdown-content");
+
+    dropdownButton.addEventListener("click", function() {
+        dropdownContent.classList.toggle("show");
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener("click", function(event) {
+        if (!event.target.matches(".dropdown .dropbtn")) {
+            if (dropdownContent.classList.contains("show")) {
+                dropdownContent.classList.remove("show");
+            }
+        }
+    });
+});
