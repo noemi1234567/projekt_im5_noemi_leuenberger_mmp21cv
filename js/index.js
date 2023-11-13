6f5781508e2d8f8bd3bbafd1cd2628cae6b76734
noemi1234567 <noemi.leuenberger@stud.fhgr.ch>
2023-11-13 15:01:48 +0100

    Player funktioniert

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Dropdown
     const dropdownButton = document.querySelector(".dropdown .dropbtn");
     const dropdownContent = document.querySelector(".dropdown .dropdown-content");
+console.log(dropdownContent)
 
     dropdownButton.addEventListener("click", function () {
         dropdownContent.classList.toggle("show");
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     loadMoreButton.addEventListener('click', function () {
         clickCount++;
+        console.log(clickCount)
         if (clickCount === 1) {
             loadMoreArticles(newArticle1);
         } else {
@@ -58,19 +60,6 @@ showSlides(slideIndex);
 setInterval(function () {
     plusSlides(1); // Advance to the next slide
 }, 3000); // 3000 milliseconds = 3 seconds 
-
-
-
-
-
-// Close the dropdown if the user clicks outside of it
-window.addEventListener("click", function (event) {
-    if (!event.target.matches(".dropdown .dropbtn")) {
-        if (dropdownContent.classList.contains("show")) {
-            dropdownContent.classList.remove("show");
-        }
-    }
-});
 
 
 // das sind alle Artikel von der Gruppe Article1
@@ -123,7 +112,7 @@ const newArticle2 = `
 
 // Funktion macht, dass die Artikel1 geladen werden 
 function loadMoreArticles(newArticle) {
-
+console.log("articles loades");
     // Get the container for additional articles
     const additionalArticlesContainer = document.getElementById("additional-articles");
 
@@ -132,21 +121,103 @@ function loadMoreArticles(newArticle) {
 }
 
 
-// event listener für den "Mehr laden" button hinzufügen
-const loadMoreButton = document.getElementById("load-more-button");
+// // event listener für den "Mehr laden" button hinzufügen
+// const loadMoreButton = document.getElementById("load-more-button");
 
-let clickCount = 0;
+// let clickCount = 0;
 
-loadMoreButton.addEventListener('click', function () {
-    clickCount++;
-    // wenn der User einmal klickt, dann werden die Artikel1 geladen
-    if (clickCount === 1) {
-        loadMoreArticles(newArticle1);
-        // loadMoreButton.addEventListener("click", loadMoreArticles1);
-    }
-    else {
-        // wenn der User zweimal klickt, dann werden die Artikel2 geladen und der Button verschwindet
-        loadMoreArticles(newArticle2);
-        loadMoreButton.style.display = 'none';
-    }
+// loadMoreButton.addEventListener('click', function () {
+//     clickCount++;
+//     // wenn der User einmal klickt, dann werden die Artikel1 geladen
+//     if (clickCount === 1) {
+//         loadMoreArticles(newArticle1);
+//         // loadMoreButton.addEventListener("click", loadMoreArticles1);
+//     }
+//     else {
+//         // wenn der User zweimal klickt, dann werden die Artikel2 geladen und der Button verschwindet
+//         loadMoreArticles(newArticle2);
+//         loadMoreButton.style.display = 'none';
+//     }
+// });
+
+// JavaScript für die Steuerung des Radioplayers
+var radioPlayer = document.getElementById('radioPlayer');
+
+function playRadio() {
+    radioPlayer.play();
+}
+
+function pauseRadio() {
+    radioPlayer.pause();
+}
+
+function stopRadio() {
+    radioPlayer.pause();
+    radioPlayer.currentTime = 0;
+}
+
+function forwardRadio() {
+    radioPlayer.currentTime += 10; // Vorwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function backwardRadio() {
+    radioPlayer.currentTime -= 10; // Rückwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function setVolume(volume) {
+    radioPlayer.volume = volume; // Volume-Wert zwischen 0 und 1 setzen
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // ... (dein bestehender Code)
+
+// Radio Player
+var radioPlayer = document.getElementById('radioPlayer');
+var playButton = document.querySelector('.play-button');
+var pauseButton = document.querySelector('.pause-button');
+var stopButton = document.querySelector('.stop-button'); // Hier füge den Stop-Button hinzu
+
+function playRadio() {
+    radioPlayer.play();
+}
+
+function pauseRadio() {
+    radioPlayer.pause();
+}
+
+function stopRadio() {
+    radioPlayer.pause();
+    radioPlayer.currentTime = 0;
+}
+
+function forwardRadio() {
+    radioPlayer.currentTime += 10; // Vorwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function backwardRadio() {
+    radioPlayer.currentTime -= 10; // Rückwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function setVolume(volume) {
+    radioPlayer.volume = volume; // Volume-Wert zwischen 0 und 1 setzen
+}
+
+// Play-Button klicken
+playButton.addEventListener('click', function () {
+    playRadio();
 });
+
+// Pause-Button klicken
+pauseButton.addEventListener('click', function () {
+    pauseRadio();
+});
+
+// Stop-Button klicken
+stopButton.addEventListener('click', function () {
+    stopRadio();
+});
+});
+
+
+
+
