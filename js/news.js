c51914002b73a980367eb1e56672121a4e46a73f
noemi1234567 <noemi.leuenberger@stud.fhgr.ch>
2023-11-14 10:37:04 +0100

    News-Player
    News-Player auf News-Seite funktioniert

--- js/news.js
+++ js/news.js
@@ -89,6 +89,83 @@ document.addEventListener("DOMContentLoaded", function() {
             loadMoreButton.style.display = 'none';
         }
     });
+});
 
-    
+// JavaScript für die Steuerung des Radioplayers
+var radioPlayer = document.getElementById('radioPlayer');
+var playButton = document.querySelector('.play')
+var pauseButton = document.querySelector('.pause')
+
+function playRadio() {
+    playButton.style.display = "none";
+    pauseButton.style.display = "block";
+    radioPlayer.play();
+}
+
+function pauseRadio() {
+    playButton.style.display = "block";
+    pauseButton.style.display = "none";
+    radioPlayer.pause();
+}
+
+function forwardRadio() {
+    radioPlayer.currentTime += 10; // Vorwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function backwardRadio() {
+    radioPlayer.currentTime -= 10; // Rückwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function setVolume(volume) {
+    radioPlayer.volume = volume; // Volume-Wert zwischen 0 und 1 setzen
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // ... (dein bestehender Code)
+
+// Radio Player
+var radioPlayer = document.getElementById('radioPlayer');
+var playButton = document.querySelector('.play-button');
+var pauseButton = document.querySelector('.pause-button');
+var stopButton = document.querySelector('.stop-button'); // Hier füge den Stop-Button hinzu
+
+function playRadio() {
+    radioPlayer.play();
+}
+
+function pauseRadio() {
+    radioPlayer.pause();
+}
+
+function stopRadio() {
+    radioPlayer.pause();
+    radioPlayer.currentTime = 0;
+}
+
+function forwardRadio() {
+    radioPlayer.currentTime += 10; // Vorwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function backwardRadio() {
+    radioPlayer.currentTime -= 10; // Rückwärts spulen um 10 Sekunden (kann angepasst werden)
+}
+
+function setVolume(volume) {
+    radioPlayer.volume = volume; // Volume-Wert zwischen 0 und 1 setzen
+}
+
+// Play-Button klicken
+playButton.addEventListener('click', function () {
+    playRadio();
+});
+
+// Pause-Button klicken
+pauseButton.addEventListener('click', function () {
+    pauseRadio();
+});
+
+// Stop-Button klicken
+stopButton.addEventListener('click', function () {
+    stopRadio();
+});
 });
